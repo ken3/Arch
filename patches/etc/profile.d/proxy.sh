c2a2fae662de88592a3f68525b5bb9d420ece3ec
TSUKAHARA, Ken <ken3@nurs.or.jp>
2018-03-28 18:41:44 +0900

    add document and patches.

---
+++ patches/etc/profile.d/proxy.sh
@@ -0,0 +1,6 @@
+export ftp_proxy=http://133.141.7.236:9515/
+export http_proxy=http://133.141.7.236:9515/
+export https_proxy=http://133.141.7.236:9515/
+export FTP_PROXY=http://133.141.7.236:9515/
+export HTTP_PROXY=http://133.141.7.236:9515/
+export HTTPS_PROXY=http://133.141.7.236:9515/
